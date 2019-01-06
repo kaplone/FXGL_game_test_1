edd9e64901942ea596df30bf0f47d07746f8488b
David <dvd.brtrnd@gmail.com>
2019-01-06 21:05:07 +0100

    chrono + niveau yaml

---
+++ src/main/java/models/But.java
@@ -0,0 +1,41 @@
+package models;
+
+public class But {
+
+    private Double tempsMax;
+    private String imagePath;
+    private Integer xPos;
+    private Integer yPos;
+
+    public Double getTempsMax() {
+        return tempsMax;
+    }
+
+    public void setTempsMax(Double tempsMax) {
+        this.tempsMax = tempsMax;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public Integer getxPos() {
+        return xPos;
+    }
+
+    public void setxPos(Integer xPos) {
+        this.xPos = xPos;
+    }
+
+    public Integer getyPos() {
+        return yPos;
+    }
+
+    public void setyPos(Integer yPos) {
+        this.yPos = yPos;
+    }
+}
